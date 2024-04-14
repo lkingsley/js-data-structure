95c29ee0d93bd1b693ada33197d26a4a8d9c3141
Luis Kingsley <lkingsley00@gmail.com>
2024-04-14 18:29:05 -0400

    Add shift function test

--- linked-list.test.js
+++ linked-list.test.js
@@ -56,3 +56,26 @@ test('Unshift elements', () => {
     expect(linkedList.length).toBe(1);
     expect(linkedList.head.value).toBe(15);
 })
+
+test('Shift elements', () => {
+    const node = new LinkedList(5);
+    node.push(10);
+    node.push(15);
+    const firstShift = node.shift();
+    
+    expect(firstShift).toEqual({ value: 5, next: null});
+    expect(node.head.value).toBe(10);
+    expect(node.length).toBe(2);
+
+    node.shift();
+    expect(node.length).toBe(1);
+    expect(node.head.next).toBe(null);
+
+    node.shift();
+    expect(node.head).toBe(null);
+    expect(node.tail).toBe(null);
+    expect(node.length).toBe(0);
+
+    expect(node.shift()).toBe(null);
+    expect(node.length).toBe(0);
+})
