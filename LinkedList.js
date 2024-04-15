13d3dbe3775a0e772a0f8bb645b8cca3754ebe4e
admin <admin@admins-MacBook-Pro.local>
2024-04-15 10:56:19 -0400

    Remove comments

--- LinkedList.js
+++ LinkedList.js
@@ -56,33 +56,7 @@ class LinkedList {
         return this;
     }
 
-    // printList() {
-    //     let temp = this.head;
-    //     while (temp !== null) {
-    //         console.log(temp.value);
-    //         temp = temp.next;
-    //     }
-    // }
 
-    // getHead() {
-    //     if (this.head === null) {
-    //         console.log("Head: null");
-    //     } else {
-    //         console.log("Head: " + this.head.value);
-    //     }
-    // }
-
-    // getTail() {
-    //     if (this.tail === null) {
-    //         console.log("Tail: null");
-    //     } else {
-    //         console.log("Tail: " + this.tail.value);
-    //     }
-    // }
-
-    // getLength() {
-    //     return this.length;
-    // }
 }
 
 
@@ -90,31 +64,3 @@ module.exports = {
     LinkedList,
     LinkedListNode
 }
-
-// function test() {
-//     let myLinkedList = new LinkedList(4);
-    
-//     myLinkedList.push(10);
-//     myLinkedList.getHead();
-//     myLinkedList.getTail();
-//     myLinkedList.getLength();
-
-//     console.log("\nLinked List:");
-//     myLinkedList.printList();
-// }
-
-
-// test();
-
-
-/*
-    EXPECTED OUTPUT:
-    ----------------
-    Head: 4
-    Tail: 4
-    Length: 1
-    
-    Linked List:
-    4
-
-*/
